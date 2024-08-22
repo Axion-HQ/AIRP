a95240dd192a0e24473478525c5af85e5518d5a7
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-08-22 14:09:19 -0400

    add tpd_small.json data (smaller file), refactor: main.py

--- rag/main.py
+++ rag/main.py
@@ -1,16 +1,14 @@
 import os
 import json
+import time
+import logging
+from enum import Enum
 from groq import Groq
 from pathlib import Path
 from openai import OpenAI
 from dotenv import load_dotenv
 from pinecone import Pinecone, ServerlessSpec
-from enum import Enum
-
-
-class AiTypes(Enum):
-    OPENAI = "text-embedding-3-small"
-    GROQ = "nomic-embed-text-v1_5"  # TODO: Groq doesn't seem to have an embedding model?
+from rich.logging import RichHandler
 
 
 class VectorDatabases(Enum):
@@ -22,10 +20,20 @@ class VectorDatabases(Enum):
 # TODO: Change the name from "axionrag" to something more meaningful
 index_reqs = {
     "name": "axionrag",
-    "dimension": 700,
+    "dimension": 1536,
     "metrics": ["cosine", "euclidean"],
     "spec": ServerlessSpec(cloud="aws", region="us-east-1")
 }
+
+# Set logging config
+logging.basicConfig(
+    format="{asctime} - {levelname}: {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.INFO,
+    handlers=[RichHandler()]
+)
+logger = logging.getLogger("rich")
 
 # Set .env location
 env_path = Path("../.env")
@@ -38,48 +46,58 @@ openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 groq = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 pine_c = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-pine_c.create_index(
-    name=index_reqs["name"],
-    dimension=index_reqs["dimension"],
-    metric=index_reqs["metrics"][0],
-    spec=index_reqs["spec"]
-)
+
+if index_reqs["name"] in pine_c.list_indexes().names():
+    logger.info(f"Deleting previous index: {index_reqs['name']}")
+    pine_c.delete_index(index_reqs["name"]) # Delete previous "axionrag" index
+
+    logger.info(f"Creating new index: {index_reqs['name']}")
+    pine_c.create_index(
+        name=index_reqs["name"],
+        dimension=index_reqs["dimension"],
+        metric=index_reqs["metrics"][0],
+        spec=index_reqs["spec"]
+    )
+
+else:
+    logger.info(f"Previous index not found. Creating new index: {index_reqs['name']}")
+
+    pine_c.create_index(
+        name=index_reqs["name"],
+        dimension=index_reqs["dimension"],
+        metric=index_reqs["metrics"][0],
+        spec=index_reqs["spec"]
+    )
 
 # Fetch unprocessed json data
-unprocessed_data = json.load(open("../data/20240819_tpd.json"))
+unprocessed_data = json.load(open("../data/20240822_tpd_small.json"))
 
 # Filter and embed data
 processed_data = []
-select_ai_type = AiTypes.OPENAI
+openai_model = "text-embedding-3-small"
 
 # TODO: Consider using other open-source vector databases like Milvus or Weaviate
-for data in unprocessed_data["professor_reviews"]:
-    response = None
-    embedding = None
-
-    if select_ai_type == AiTypes.GROQ:
-        response = groq.embeddings.create(
-            input=data["review"],
-            model=AiTypes.GROQ.value
-        )
-
-    elif select_ai_type == AiTypes.OPENAI:
-        response = openai.embeddings.create(
-            input=data["review"],
-            model=AiTypes.OPENAI.value,
-        )
+for idx, data in enumerate(unprocessed_data["professor_reviews"]):
+    response = openai.embeddings.create(
+        input=data["review"],
+        model=openai_model
+    )
 
     embedding = response.data[0].embedding
+    logger.info(f"Embedded data: {embedding}")
+
     processed_data.append({
         "values": embedding,
         "id": data["professor"],
         "metadata": {
-            "department": data["departement"],
+            "department": data["department"],
             "rating": data["rating"],
             "review": data["review"],
             "timestamp": data["timestamp"],
         }
     })
+    time.sleep(5)  # To prevent HTTP Request: POST https://api.openai.com/v1/embeddings "HTTP/1.1 429 Too Many Requests"
+    logger.info(f"Processed embedded data: {processed_data[idx]}")
 
 # Insert data into vector database (current: Pinecone)
 pine_c_index = pine_c.Index("axionrag")
@@ -87,6 +105,7 @@ pine_c_index.upsert(
     vectors=processed_data,
     namespace="arag",
 )
+logger.info(f"Inserted data into Pinecone index: {index_reqs['name']}")
 
 # Describe the index with the inserted data
 print(pine_c_index.describe_index_stats())
