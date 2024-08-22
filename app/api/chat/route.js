50599c97f36592a2d89b5b8c10c491ae5e8236df
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-08-22 14:28:27 -0400

    refactor: route.js api/chat (fixed minor bugs)

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,23 +1,23 @@
-import  { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { Pinecone } from "@pinecone-database/pinecone";
 import OpenAI from "openai";
 import Groq from "groq-sdk";
 
 const pinecone = new Pinecone({
-    apiKey: process.env.PINECONE_API_KEY,
-    additionalHeaders: {
-        'Content-Type': 'application/json',
-    }
+	apiKey: process.env.PINECONE_API_KEY,
+	additionalHeaders: {
+		"Content-Type": "application/json",
+	},
 });
 
-
+// Set OPENAI Configuration
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
-
+// Set GROQ Configuration
 const groq = new Groq({
-    apiKey: process.env.GROQ_API_KEY,
+	apiKey: process.env.GROQ_API_KEY,
 });
 
 // TODO: Edit systemPrompt iteratively to improve AI results.
@@ -60,77 +60,69 @@ Clarity and Efficiency:
 By following these guidelines, your goal is to assist users in making informed and confident decisions when selecting professors, ultimately enhancing their academic experience.
 `;
 
-
 export async function POST(req) {
-    const data = await req.json();
-    const index = pinecone.index("axionrag").namespace("arag");
+	const data = await req.json();
+	const index = pinecone.index("axionrag").namespace("arag");
 
-    const text = data[data.length - 1].content
-    const embedding = await openai.embeddings.create({
-        model: "text-embedding-3-small",
-        input: text,
-        encoding_format: "float"
-    });
+	const text = data[data.length - 1].content;
+	const embedding = await openai.embeddings.create({
+		model: "text-embedding-3-small",
+		input: text,
+		encoding_format: "float",
+	});
 
-    const response = await index.query({
-        topK: 3, 
-        includeMetadata: true,
-        vector: embedding.data[0].embedding
-    })
+	const response = await index.query({
+		topK: 3,
+		includeMetadata: true,
+		vector: embedding.data[0].embedding,
+	});
 
-    let responseString = "";
-    response.matches.forEach((match) => {
-        responseString += `  
+	let responseString = "";
+	response.matches.forEach((match) => {
+		responseString += `  
         Professor: ${match.id},
         Department: ${match.metadata?.department}
         Rating: ${match.metadata?.rating}
         Review: ${match.metadata?.review}
         Timestamp: ${match.metadata?.timestamp}
         \n
-        `
-    });
+        `;
+	});
 
-    const lastMessage = data[data.length - 1].content;
-    const lastMessageContent = lastMessage.content + responseString;
-    const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+	const lastMessage = data[data.length - 1];
+	const lastMessageContent = lastMessage.content + responseString;
+	const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
 
-    const completion = await openai.completions.create({
-        messages: [
-            {
-                role: "system",
-                content: systemPrompt,
-            },
-            ...lastDataWithoutLastMessage,
-            {
-                role: "user",
-                content: lastMessageContent
-            },
-        ],
-        model: "gpt-4o-mini",
-        stream: true
-    });
+	const completion = await openai.chat.completions.create({
+		messages: [
+			{ role: "system", content: systemPrompt },
+			...lastDataWithoutLastMessage,
+			{ role: "user", content: lastMessageContent },
+		],
+		model: "gpt-3.5-turbo",
+		stream: true,
+	});
 
-    const stream = ReadableStream({
-        async start(controller) {
-            const encoder = new TextEncoder();
+	const stream = new ReadableStream({
+		async start(controller) {
+			const encoder = new TextEncoder();
 
-            try {
-                for await(const chunk of completion) {
-                    const content = chunk.choices[0]?.delta.content;
+			try {
+				for await (const chunk of completion) {
+					const content = chunk.choices[0]?.delta.content;
 
-                    if (content) {
-                        const text = encoder.encode(content);
-                        controller.enqueue(text);
-                    }
-                }
-            } catch (error) {
-                controller.error(error);
+					if (content) {
+						const text = encoder.encode(content);
+						controller.enqueue(text);
+					}
+				}
+			} catch (error) {
+				controller.error(error);
+			} finally {
+				controller.close();
+			}
+		},
+	});
 
-            } finally {
-                controller.close();
-            }
-        }
-    })
-
-    return new NextResponse(stream);
+	return new NextResponse(stream);
 }
